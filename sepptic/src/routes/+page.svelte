a1b13140da0c894358913d5f152583622f74b285
porcupineparty <matlanet@gmail.com>
2024-10-08 08:21:42 -0600

    Button

--- sepptic/src/routes/+page.svelte
+++ sepptic/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+    import Button from '$lib/components/atoms/Button.svelte';
+
+</script>
+<h1>custom button demo</h1>
+<!-- Default button -->
+<Button />
+
+<!--Props-->
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>

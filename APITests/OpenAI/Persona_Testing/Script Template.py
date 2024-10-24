899c57a9aa736ea4d38f4ee36dbb718bfa874a1d
elliotdelay <155603013+elliotdelay@users.noreply.github.com>
2024-10-24 10:16:32 -0600

    Create Script Template.py

---
+++ APITests/OpenAI/Persona_Testing/Script Template.py
@@ -0,0 +1,51 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+from typing_extensions import override
+from openai import AssistantEventHandler
+#
+# Add description of persona here
+#
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+elara = client.beta.assistants.create(
+    model='gpt-4o',
+)
+
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nElara: ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+
+
+thread = client.beta.threads.create()
+
+def chat():
+  while True:
+    prompt=input("\n\nYou: ")
+    if prompt.lower() == 'exit':
+      break
+    
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=prompt
+    )
+
+    with client.beta.threads.runs.stream(
+        thread_id=thread.id,
+        assistant_id=elara.id,
+        # Change the instructions to alter the initial prompt #########################
+        instructions='',
+        event_handler=EventHandler(),
+    ) as stream:
+        stream.until_done()
+
+if __name__ == "__main__":
+    chat()

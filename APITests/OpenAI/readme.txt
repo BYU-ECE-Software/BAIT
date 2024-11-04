ce9c309ef5bafc3d0e84bee946dfb8514289e06d
rootbeerefloat <ethanbeere123@gmail.com>
2024-11-04 08:47:59 -0700

    Reapply "Merge branch 'main' of https://github.com/rootbeerefloat/SEPPTIC"
    This reverts commit fa42d7923bd73022c2182c7b30f2cf37afb10c6c.

--- APITests/OpenAI/readme.txt
+++
@@ -1,3 +0,0 @@
-pip install openai
-
-To use the script, you need to add the API key as a path variable. I will explain in future versions.
--- sepptic/src/lib/index.ts
+++ sepptic/src/lib/index.ts
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as RootButton } from './components/atoms/rootElements/RootButton.svelte';
 export { default as ButtonOne } from './components/atoms/inheritedElements/ButtonOne.svelte';
+export { default as InputField } from './components/atoms/rootElements/InputField.svelte';

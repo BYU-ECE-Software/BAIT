b8185e0cb0004df1c47ac4c36c48e533031b6ca1
Garrett White <garrettmwhite01@gmail.com>
2024-11-18 09:26:21 -0700

    Added Elara's Card as molecule

--- sepptic/src/routes/harvesta/+page.svelte
+++ sepptic/src/routes/harvesta/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { Tabs, TabItem, Button, Card } from 'flowbite-svelte';
-    import {HarvestaVideoPlayer, HarvestaFoodsCard, SideBarButton} from '$lib';
+    import {HarvestaVideoPlayer, HarvestaFoodsCard, SideBarButton, ElaraSmallCard} from '$lib';
   </script>
   <SideBarButton />
   <Tabs tabStyle="underline">
@@ -24,8 +24,7 @@
         <img src="/HarvestaOfficeFloorplan.jpg" alt="Office Floorplan"/>
         <div class="content" style=" position: absolute; top: 70%; left: 30%;">
           <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Elara</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">Receptionist</p>
+            <ElaraSmallCard />
           </a>
         </div>
         <div class="content" style=" position: absolute; top: 35%; left: 75%;">

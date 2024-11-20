714cc721e46a652f82b481286e1c316709cb10f2
rootbeerefloat <ethanbeere123@gmail.com>
2024-11-20 09:45:06 -0700

    Implement get campaigns

---
+++ sepptic/src/server/utils/jsonGetCampaigns.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+function getCampaignsFiles() {
+    const campaignsDir = '../campaigns';
+    try {
+        const files = fs.readdirSync(campaignsDir);
+        return files.filter(file => fs.statSync(path.join(campaignsDir, file)).isFile());
+    } catch (error) {
+        console.error('Error reading campaigns directory:', error);
+        return [];
+    }
+};
+
+function getCampaign(fileName: string) {
+    const campaignsDir = '../campaigns';
+    const file = path.join(campaignsDir, fileName);
+    try {
+        const data = JSON.parse(fs.readFileSync(file).toString());
+        const campaignData = {
+            id: fileName.split('.')[0],
+            name: data.Campaign_Information.Name,
+            description: data.Campaign_Information.Description,
+            image: data.Campaign_Information.Image
+        }
+        return campaignData;
+    } catch (error) {
+        console.error('Error reading campaign file:', error);
+        return {};
+    }
+}
+
+export default function jsonGetCampaigns() {
+    const files = getCampaignsFiles();
+    return files.map(getCampaign);
+}

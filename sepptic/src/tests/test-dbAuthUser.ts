ef3754855b5d9a5669b5d901585401d8c0b9d031
hansenzeke <hansenzeke22@gmail.com>
2024-11-20 09:31:25 -0700

    Update test email

--- sepptic/src/tests/test-dbAuthUser.ts
+++ sepptic/src/tests/test-dbAuthUser.ts
@@ -1,7 +1,7 @@
 import dbAuthUser from '../server/utils/dbAuthUser';
 
 // Test that the function dbAuthUser returns true or false when given valid input.
-const email = 'JD24@gmail.com';
+const email = 'test@example.com';
 const password = 'UVU2024!';
 const result = await dbAuthUser(email, password);
 console.log(result);

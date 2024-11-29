51ea3060ceab0ad0f02d1e24840c1e96d3919d6a
porcupineparty <matlanet@gmail.com>
2024-11-29 14:39:28 -0700

    Card size changes for uniformity

--- sepptic/src/lib/components/molecules/LearnCard.svelte
+++ sepptic/src/lib/components/molecules/LearnCard.svelte
@@ -2,13 +2,19 @@
     import { Card, Button } from 'flowbite-svelte';
 </script>
 
-<div  class="w-full max-w-3xl mx-auto">
-    <Card img="/SocialEngineering.jpeg" class="w-full">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Learn Social Engineering</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here you can learn more about the art of Social Engineering to better help you tackle even our hardest Campaigns.</p>
-        <Button class="dark:hover:bg-seppticOrange-700 bg-seppticOrange-600 dark:bg-seppticOrange-600 hover:bg-seppticOrange-700 text-white focus:outline-none w-full " href="/main/learn">
+<div class="mx-auto" style="max-width: 768px;">
+    <Card img="/SocialEngineering.jpeg" class="w-[768px]">
+        <h5 class="mb-2 text-[24px] font-bold tracking-tight text-gray-900 dark:text-white">
+            Learn Social Engineering
+        </h5>
+        <p class="mb-3 text-[16px] font-normal text-gray-700 dark:text-gray-400 leading-[1.5]">
+            Here you can learn more about the art of Social Engineering to better help you tackle even our hardest Campaigns.
+        </p>
+        <Button
+                class="dark:hover:bg-seppticOrange-700 bg-seppticOrange-600 dark:bg-seppticOrange-600 hover:bg-seppticOrange-700 text-white focus:outline-none w-full"
+                href="/main/learn"
+        >
             Learn Here
         </Button>
     </Card>
-
 </div>

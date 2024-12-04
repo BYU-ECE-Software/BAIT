1d645358d6678301749351e1673a40192a754cd7
Garrett White <garrettmwhite01@gmail.com>
2024-12-04 09:50:02 -0700

    Updated main dashboard with actual storyline names and job titles

--- sepptic/src/routes/harvesta/+page.svelte
+++ sepptic/src/routes/harvesta/+page.svelte
@@ -29,14 +29,14 @@
         </div>
         <div class="content" style=" position: absolute; top: 35%; left: 75%;">
           <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ben Dover</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">Finance Guy</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tony Flagg</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Real Estate Intern</p>
           </a>
        </div>
        <div class="content" style=" position: absolute; top: 25%; left: 10%;">
           <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Don Draper</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">Projects</p>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Project Manager</p>
           </a>
         </div>
         <div class="content" style=" position: absolute; top: 65%; left: 5%;">
@@ -47,8 +47,8 @@
         </div>
         <div class="content" style=" position: absolute; top: 65%; left: 75%;">
           <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Interns</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">MVP MVP MVP MVP MVP</p>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ann Gunn</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">Finance Associate</p>
           </a>
         </div>
     </div>

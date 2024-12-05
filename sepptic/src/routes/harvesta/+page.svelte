c22dbe5e631293a8aa96d9b143d9f841352fcc36
Garrett White <garrettmwhite01@gmail.com>
2024-12-05 09:30:07 -0700

    Adjust Elara card on main dashboard of Harvesta Campaign

--- sepptic/src/routes/harvesta/+page.svelte
+++ sepptic/src/routes/harvesta/+page.svelte
@@ -23,7 +23,7 @@
       <div class="wrap" style="display: inline-block; position: relative;">
         <img src="/HarvestaOfficeFloorplan.jpg" alt="Office Floorplan"/>
         <div class="content" style=" position: absolute; top: 70%; left: 30%;">
-          <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+          <a href="#" class="block max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <ElaraSmallCard />
           </a>
         </div>

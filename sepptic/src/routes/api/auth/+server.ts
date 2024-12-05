f7b29e56750ae6a9dcf985097fe8f995ef378815
rootbeerefloat <ethanbeere123@gmail.com>
2024-12-05 13:38:34 -0700

    Switched to 🍪's

--- sepptic/src/routes/api/auth/+server.ts
+++ sepptic/src/routes/api/auth/+server.ts
@@ -16,7 +16,32 @@ export async function POST(event: RequestEvent) {
         return new Response(JSON.stringify(response), { status: result.status });
     }
     const sessionResponse = await dbCreateSession(result.userId);
-    return new Response(JSON.stringify(sessionResponse), { status: sessionResponse.status });
+    // return new Response(JSON.stringify(sessionResponse), { status: sessionResponse.status });
+
+    // Check if the session was created
+    if (!sessionResponse.token) {
+        const response = {
+            sessionId: null,
+            message: sessionResponse.message,
+            status: sessionResponse.status
+        }
+        return new Response(JSON.stringify(response), { status: sessionResponse.status });
+    }
+
+    // Set cookie with token
+    const cookie = `token=${sessionResponse.token}; HttpOnly; Secure; Path=/; Max-Age=${Math.floor((sessionResponse.expiration.getTime() - Date.now()) / 1000)}`;
+
+    const sessionStatus = {
+        message: sessionResponse.message,
+        status: sessionResponse.status
+    }
+
+    return new Response(JSON.stringify(sessionStatus), {
+        status: sessionResponse.status,
+        headers: {
+            'Set-Cookie': cookie
+        }
+    });
 }
 
 export async function DELETE(event: RequestEvent) {

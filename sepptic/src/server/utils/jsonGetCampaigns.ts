600b8e489042aaaa287997934fad79e1bb539189
rootbeerefloat <ethanbeere123@gmail.com>
2025-01-16 08:52:37 -0700

    Moved campaign type to its own file

--- sepptic/src/server/utils/jsonGetCampaigns.ts
+++ sepptic/src/server/utils/jsonGetCampaigns.ts
@@ -1,37 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-
-type campaign = {
-    Campaign_Information: {
-        Name: string;
-        Description: string;
-        Image: string;
-        Website: string;
-        Briefing_Video: string;
-        Failure_Video: string;
-    };
-    Characters: {
-        ID: number;
-        Name: string;
-        Title: string;
-        Image: string;
-        Prompt: string;
-        Intel: {
-            Intel_ID: number;
-            Intel_Description: string;
-            Unlocks_Character_ID?: number;
-            Quiz: string;
-            Answer: string;
-        }[];
-    }[];
-    Achievements: {
-        ID: number;
-        Name: string;
-        Description: string;
-        Image: string;
-        Intels: number[];
-    }[];
-}
+import type { campaign } from './types/campaign';
 
 const campaignsDir = './src/server/campaigns';
 
---
+++ sepptic/src/server/utils/types/campaign.ts
@@ -0,0 +1,31 @@
+export type campaign = {
+    Campaign_Information: {
+        Name: string;
+        Description: string;
+        Image: string;
+        Website: string;
+        Briefing_Video: string;
+        Failure_Video: string;
+    };
+    Characters: {
+        ID: number;
+        Name: string;
+        Title: string;
+        Image: string;
+        Prompt: string;
+        Intel: {
+            Intel_ID: number;
+            Intel_Description: string;
+            Unlocks_Character_ID?: number;
+            Quiz: string;
+            Answer: string;
+        }[];
+    }[];
+    Achievements: {
+        ID: number;
+        Name: string;
+        Description: string;
+        Image: string;
+        Intels: number[];
+    }[];
+}

25e608f6674158de019270899bd766fb2e1362fa
porcupineparty <matlanet@gmail.com>
2025-01-30 09:29:58 -0700

    Fixed another bug

--- sepptic/src/routes/harvesta/+page.svelte
+++ sepptic/src/routes/harvesta/+page.svelte
@@ -371,7 +371,7 @@
         </div>
     {/if}
 
-    <BottomNav position="fixed" classInner="grid-cols-5" activeUrl="/" style="bottom-0 left-0 right-0 z-10">
+    <BottomNav position="fixed" classInner="grid-cols-5" activeUrl="/" style="bottom-0: left-0 right-0 z-10">
         <BottomNavItem btnName="Home" on:click={() => selectedTab = 'Home'}>
             <HomeSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
         </BottomNavItem>

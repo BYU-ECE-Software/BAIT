0709282521f60dfc68c59b06e8b18d362ed905f0
rootbeerefloat <ethanbeere123@gmail.com>
2025-02-05 09:09:10 -0700

    Calculate progresses

--- sepptic/src/routes/main/campaigns/[slug]/+page.server.ts
+++ sepptic/src/routes/main/campaigns/[slug]/+page.server.ts
@@ -1,5 +1,29 @@
 import type { PageServerLoad } from "./$types";
 
+function calculateProgresses(userProgress: any, campaign: any, slug: string) {
+    let progresses = {
+        'campaign': 0,
+        'characters': {}
+    };
+    if (!userProgress.intelByCampaign[slug]){
+        return progresses;
+    }
+    const userTotalIntel = userProgress.intelByCampaign[slug].length;
+    const totalCampaignIntel = campaign.Campaign_Information.Total_Intel;
+    const campaignProgress = Math.floor((userTotalIntel / totalCampaignIntel) * 100);
+    progresses['campaign'] = campaignProgress;
+    
+    let characterProgresses: { [key: string]: number } = {};
+    campaign.Characters.forEach((character: any) => {
+        const characterIntel = character.Total_Intel;
+        const userCharacterIntel = userProgress.intelByCampaign[slug].filter((intel: any) => intel.Character_ID === character.ID).length;
+        const characterProgress = Math.floor((userCharacterIntel / characterIntel) * 100);
+        characterProgresses[character.ID] = characterProgress;
+    });
+    progresses['characters'] = characterProgresses;
+    return progresses;
+}
+
 export const load: PageServerLoad = async ({ params, fetch, cookies }) => {
     const sessionToken = cookies.get('token');
     const slug = params.slug;
@@ -9,9 +33,14 @@ export const load: PageServerLoad = async ({ params, fetch, cookies }) => {
     const userResponse = await fetch('/api/profile', {
         headers: { 'Authorization': `Bearer ${sessionToken}` }
     });
+    const userProgressResponse = await fetch('/api/progress', {
+        headers: { 'Authorization': `Bearer ${sessionToken}` }
+    })
+    const userProgress = await userProgressResponse.json();
     const campaignJson = await campaignsResponse.json();
     const userJson = await userResponse.json();
     const campaign = campaignJson.data;
     const user = userJson;
-    return { campaign, user, slug };
+    const progresses = calculateProgresses(userProgress, campaign, slug);
+    return { campaign, user, slug, progresses };
 };

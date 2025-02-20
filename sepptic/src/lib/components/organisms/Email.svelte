f302f0418e1ced8b7e85d41f7d2bd0387bf8368c
porcupineparty <matlanet@gmail.com>
2025-02-20 08:23:23 -0700

    Adding Email Functionality v1.0

---
+++ sepptic/src/lib/components/organisms/Email.svelte
@@ -0,0 +1,26 @@
+
+<script lang="ts">
+    import { EmailSidebar } from '$lib';
+    import { EmailHeader } from "$lib";
+    import { EmailList } from "$lib";
+    import { EmailCompose } from "$lib";
+    import { EmailView } from "$lib";
+
+    let isComposing = false;
+    let selectedEmail = null;
+</script>
+
+<div class="flex h-screen">
+    <EmailSidebar />
+    <div class="flex flex-col flex-1">
+        <EmailHeader />
+        <EmailList on:select={(e) => selectedEmail = e.detail} />
+        {#if selectedEmail}
+            <EmailView email={selectedEmail} on:close={() => selectedEmail = null} />
+        {/if}
+    </div>
+</div>
+
+{#if isComposing}
+    <EmailCompose on:close={() => isComposing = false} />
+{/if}

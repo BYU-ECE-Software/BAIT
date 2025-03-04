53294bc64cb6e236a65c93289688a1233eac3253
rootbeerefloat <ethanbeere123@gmail.com>
2025-03-04 09:20:26 -0700

    Add video

---
+++ sepptic/src/lib/components/molecules/YoutubeVideoCard.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let src;
+</script>
+
+<iframe width="1218" height="685" src="{src}" title="Briefing Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>

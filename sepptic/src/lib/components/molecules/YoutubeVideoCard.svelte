25d8f192ef12b7f92fc55fc5b8725a1e9d535982
rootbeerefloat <ethanbeere123@gmail.com>
2025-03-05 09:33:10 -0700

    Fixed Video Size

--- sepptic/src/lib/components/molecules/YoutubeVideoCard.svelte
+++ sepptic/src/lib/components/molecules/YoutubeVideoCard.svelte
@@ -2,4 +2,4 @@
     export let src;
 </script>
 
-<iframe width="75vw" height="auto" src="{src}" title="Briefing Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="1218" height="685" src="{src}" title="Briefing Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>

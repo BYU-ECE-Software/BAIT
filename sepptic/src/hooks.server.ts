b9d8236f162181cc7bc429eb83ed741ebe1fa582
Jknepper1 <Jsknepper10@gmail.com>
2025-08-08 11:11:49 -0600

    updated scheme to match new role column in user; built server hook for checking user role

--- sepptic/src/hooks.server.ts
+++ sepptic/src/hooks.server.ts
@@ -5,3 +5,28 @@
 //   return resolve(event);
 // };
 // Used to test for CSRF issues and same-origin mismatches
+import type { Handle } from '@sveltejs/kit'
+import dbGetUser from './server/utils/dbGetUser';
+import getUserIdFromToken from './server/utils/getUserIdFromToken'
+
+export const handle: Handle = async({ event, resolve }) => {
+    const token = event.cookies.get("token");
+    if (!token) {
+        event.locals.role = null;
+        return resolve(event);
+    }
+    const tokenResponse = await getUserIdFromToken(token); 
+    if (!tokenResponse.success || tokenResponse.userId) {
+        event.locals.role = null
+        return resolve(event)
+    }
+    const userId = tokenResponse.userId;
+    const userResponse = await dbGetUser(userId);
+    if (userResponse.status !== 200|| !userResponse.user) {
+        event.locals.role = null
+        return resolve(event)
+    }
+    event.locals.role = userResponse.user.role;
+    console.log(event.locals.role)
+    return resolve(event);
+}

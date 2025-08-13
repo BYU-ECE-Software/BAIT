77234763af5ead46cf3c81bd9387fc43a79cdc28
Jknepper1 <Jsknepper10@gmail.com>
2025-08-13 10:33:04 -0600

    fixed a potential rac condition in auto call end; completed responses API migration

--- sepptic/src/server/utils/aiSendMessage.ts
+++ sepptic/src/server/utils/aiSendMessage.ts
@@ -1,7 +1,9 @@
-import type { message, apiCall } from './types/aiApi';
 import OpenAI from 'openai';
 
-// Function to send a message to the ChatGPT API. Takes in an array of messages, the message to send, and the prompt to send.
+
+let previousId: string;
+
+// Function to send a message to the ChatGPT API. Takes in the message to send and the prompt to send.
 export default async function aiSendMessage(message: string, prompt: string) {
     const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
     
@@ -12,11 +14,19 @@ export default async function aiSendMessage(message: string, prompt: string) {
         store: true,
         reasoning: {
             effort: "low", // Prevents GPT-5 from taking forever to respond
-        }
+        },
+        previous_response_id: previousId
     })
 
-    console.log(response.output_text);
-    console.log(response.previous_response_id);
+    console.log("The previous id is: ", previousId)
+
+    previousId = response.id;
+
+    console.log("The current id: ", response.id);
+
+    console.log("The previous id has been set to: ", previousId);
+    
+
     return response.output_text;
     
 }
